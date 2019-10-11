d4fd2e46e26ce4532bc84c6e693a7cf414cd2a39
John Neffenger <john@status6.com>
2019-10-11 11:16:44 -0700

    Simplify scripts updating the local Maven cache
    mvninstall.sh
    - Upgrade to OpenJFX version 14-dev.
    - Install all OpenJFX modules.
    
    syncopenjfx.sh
    - Don't delete existing artifacts in the cache.

--- bin/mvninstall.sh
+++ bin/mvninstall.sh
@@ -1,27 +1,24 @@
 #!/bin/bash
+# Installs the OpenJFX SDK artifacts into the local repository
+#
 # Guide to installing 3rd party JARs
 # https://maven.apache.org/guides/mini/guide-3rd-party-jars-local.html
 #
-# After running this script, right-click the JAR file under Dependencies
-# in the NetBeans Projects view and manually install the artifact again.
+# Add javafx-base and javafx-graphics as dependencies and run with:
+#   $ java -Djava.library.path=$HOME/lib/javafx-sdk-14-dev/lib \
+#       -jar target/benchmarks.jar
+# This script assumes the OpenJFX sources have been extracted with:
+#   $ unzip -q $HOME/lib/javafx-sdk-14-dev/lib/src.zip \
+#       -d $HOME/lib/javafx-sdk-14-dev/src
 trap exit INT TERM
 set -o errexit
 
-export JAVA_HOME=$HOME/opt/jdk-12.0.1
-sdk=$HOME/lib/javafx-sdk-13-dev
-mvn=/snap/netbeans/current/netbeans/java/maven/bin/mvn
+sdk=$HOME/lib/javafx-sdk-14-dev
+ver=14-dev
 
-$mvn -version
-
-jar -cf $sdk/src/javafx-base-13-dev-sources.jar -C $sdk/src/javafx.base .
-jar -cf $sdk/src/javafx-graphics-13-dev-sources.jar -C $sdk/src/javafx.graphics .
-
-$mvn install:install-file -DgeneratePom=false -Dfile=$sdk/lib/javafx.base.jar \
-    -DgroupId=org.openjfx -DartifactId=javafx-base -Dversion=13-dev -Dpackaging=jar
-$mvn install:install-file -DgeneratePom=false -Dfile=$sdk/src/javafx-base-13-dev-sources.jar \
-    -DgroupId=org.openjfx -DartifactId=javafx-base -Dversion=13-dev -Dpackaging=jar
-
-$mvn install:install-file -DgeneratePom=false -Dfile=$sdk/lib/javafx.graphics.jar \
-    -DgroupId=org.openjfx -DartifactId=javafx-graphics -Dversion=13-dev -Dpackaging=jar
-$mvn install:install-file -DgeneratePom=false -Dfile=$sdk/src/javafx-graphics-13-dev-sources.jar \
-    -DgroupId=org.openjfx -DartifactId=javafx-graphics -Dversion=13-dev -Dpackaging=jar
+for mod in base controls fxml graphics media swing web; do
+    jar -cf $sdk/src/javafx.$mod.jar -C $sdk/src/javafx.$mod .
+    mvn install:install-file -Dfile=$sdk/lib/javafx.$mod.jar \
+        -Dsources=$sdk/src/javafx.$mod.jar -Dpackaging=jar \
+        -DgroupId=org.openjfx -DartifactId=javafx-$mod -Dversion=$ver
+done
--- bin/syncopenjfx.sh
+++ bin/syncopenjfx.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
-# Updates the local OpenJFX Maven artifacts from the build machine
+# Updates the local repository with the latest OpenJFX artifacts
+#
+# First publish the artifacts locally on the build machine with:
+#   $ gradle publishToMavenLocal
+# Add javafx-graphics as a dependency and run with:
+#   $ java -Djava.library.path=$HOME/lib/javafx-sdk-14-dev/lib \
+#       -jar target/benchmarks.jar
 trap exit INT TERM
 set -o errexit
 
+host=buildjfx64
 dir=.m2/repository/org/openjfx
-rsync -av --delete buildjfx64:$dir/ $HOME/$dir/
+rsync -av $host:$dir/ $HOME/$dir/
